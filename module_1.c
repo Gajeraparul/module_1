ef9cd421c382c3ada2477d2805d887d848e8f930
Gajeraparul <advparulbutani@gmail.com>
2023-04-29 11:12:36 +0530

    module 1 is compel

---
+++ module_1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("My Name Is : Gajera Parul\n");
+    printf("My Birth Date Is : 1st May 1995\n");
+    printf("My Age Is : 28\n");
+    printf("My Address Is : A-65,Yogiraj Society,Nr Mahavir Circle,Yogi Chowk,Surat-395006\n");
+
+}
